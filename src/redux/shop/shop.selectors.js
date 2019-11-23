9bcff2e3b469aa8aab1bedc533bbaf9931f70b04
eric.stanleyjones@gmail.com <eric.stanleyjones@gmail.com>
2019-11-23 13:22:10 +0530

    Updated shop selector

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -12,16 +12,6 @@ export const selectCollectionsForPreview = createSelector(
     collections => Object.keys(collections).map(key => collections[key])
 )
 
-// export const selectCollection = collectionUrlParam => createSelector(
-//     [selectCollections],
-//     collections => {
-//         let collection = collections.filter(collection => 
-//             collection.title.toLowerCase() === collectionUrlParam)
-//         collection = Object.keys(collection).map(key => collection[key])[0]
-//         return collection
-//     }
-// )
-
 export const selectCollection = collectionUrlParam => createSelector(
     [selectCollections],
     collections => collections[collectionUrlParam]
